37c39e846a4db263b2b753bc4312b0f903da179b
prateek-pratu <prateekmkulkarni01+github@gmail.com>
2025-09-19 07:41:21 +0530

    Update vite.config.js
    configuring to use absolute path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'; // Import 'path' module
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Alias '@' to the 'src' directory
+    },
+  },
   plugins: [react(), tailwindcss()],
-})
+});
