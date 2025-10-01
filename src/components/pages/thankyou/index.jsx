7ac0c3e9d5b3368a668ed848e70eba902f2bfe7c
prateek-pratu <prateekmkulkarni01+github@gmail.com>
2025-10-01 07:03:02 +0530

    Page(feat): added thank you page

--- src/components/pages/thankyou/index.jsx
+++ src/components/pages/thankyou/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import thank from "@/assets/thankyou.svg"
 
 const Thankyou = () => {
   return (
-    <div>Thankyou</div>
+    <div className='w-[75vw] p-25'>
+      <img className="aspect-rectangle w-100" src={thank} alt="Thank_you_image" />
+      <div className='w-full flex justify-center mt-10 text-accent2-500'>
+        <h2 className='h2_main'>Please Visit Again</h2>
+        </div>
+    </div>
   )
 }
 
